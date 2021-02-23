ada350080c9c8f98af3348cefe7d40e040d4fd65
theMage <themage@magick-source.net>
2021-02-23 18:08:22 +0100

    add a strings utils - urify
    themage

---
+++ lib/SorWeTo/Utils/String.pm
@@ -0,0 +1,24 @@
+package SorWeTo::Utils::String;
+
+use Mojo::Base -strict;
+use parent 'Exporter';
+
+use Text::Unidecode qw(unidecode);
+
+our @EXPORT_OK = qw(
+    urify
+  );
+
+sub urify {
+  my ($text) = @_;
+
+  my $url = unidecode( $text );
+
+  $url =~ s{\W}{-}g;
+  $url =~ s{\-+}{-}g;
+  $url =~ s{\-+\z}{};
+
+  return $url;
+}
+
+1;
