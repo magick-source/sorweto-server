827a8676332899092ac54ea49417db0b1e8b93f1
theMage <themage@magick-source.net>
2021-03-24 13:12:54 +0100

    add search_where and search_where_count
    themage

--- lib/SorWeTo/Db.pm
+++ lib/SorWeTo/Db.pm
@@ -1,7 +1,13 @@
 package SorWeTo::Db;
 
 use Mojo::Base 'Class::DBI';
-use Class::DBI::AbstractSearch;
+use SQL::Abstract::Limit;
+
+__PACKAGE__->set_sql(retrieve_single => <<EoQ );
+SELECT  %s
+FROM    __TABLE__
+WHERE   %s
+EoQ
 
 # TODO: create an insert_or_update method that will use UDKU
 
@@ -43,6 +49,40 @@ sub flagged {
       if $flags{ $flag };
   }
   return @flagged;
+}
+
+sub search_where {
+  my $class = shift;
+
+  my ($phrase, @bind) = $class->_where( @_ );
+
+  return $class->retrieve_from_sql( $phrase, @bind );
+}
+
+sub search_where_count {
+  my $class = shift;
+
+  my ($phrase, @bind) = $class->_where( @_ );
+
+  my ($count) = $class->sql_retrieve_single( 'COUNT(*)', $phrase )
+                      ->select_val( @bind );
+
+  return $count;
+}
+
+sub _where {
+  my $class = shift;
+  my $where = (ref $_[0]) ? $_[0]          : { @_ };
+  my $attr  = (ref $_[0]) ? $_[1]          : undef;
+  my $order = ($attr)     ? delete($attr->{order_by}) : undef;
+  my $limit  = ($attr)    ? delete($attr->{limit})    : undef;
+  my $offset = ($attr)    ? delete($attr->{offset})   : undef;
+
+  my $sql = SQL::Abstract::Limit->new(%$attr);
+  my($phrase, @bind) = $sql->where($where, $order, $limit, $offset);
+  $phrase =~ s/^\s*WHERE\s*//i;
+  
+  return ($phrase, @bind);
 }
 
 sub do_transaction {
